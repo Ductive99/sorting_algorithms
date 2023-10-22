03c7d393be4395bd8d794f751306fd359d77a0e1
Alex Sifuna <aleksifuna@gmail.com>
2023-10-22 09:44:27 +0000

    fixed an issue with the quicksort algo and the big O notation

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,17 +4,16 @@
  * @array: array containing the elements to swap
  * @ind1: index to first element in array
  * @ind2: index to secont element in array
+ * @size: size of array to be swapped
  */
-void swap(int *array, int ind1, int ind2)
+void swap(int *array, int ind1, int ind2, size_t size)
 {
 	int temp;
-	size_t n;
 
-	n = 10;
 	temp = array[ind1];
 	array[ind1] = array[ind2];
 	array[ind2] = temp;
-	print_array(array, n);
+	print_array(array, size);
 }
 
 /**
@@ -22,10 +21,11 @@ void swap(int *array, int ind1, int ind2)
  * @array: array to be partitioned
  * @low: lower bound of the array
  * @high: upper bound of the array
+ * @size: the size of array to be partitioned
  *
  * Return: partition index
  */
-int partition(int *array, int low, int high)
+int partition(int *array, int low, int high, size_t size)
 {
 	int i, j, pivot;
 
@@ -36,11 +36,11 @@ int partition(int *array, int low, int high)
 		if (array[j] < pivot)
 		{
 			if (j != i)
-				swap(array, i, j);
+				swap(array, i, j, size);
 			i++;
 		}
 	}
-	swap(array, i, high);
+	swap(array, i, high, size);
 	return (i);
 }
 /**
@@ -48,16 +48,17 @@ int partition(int *array, int low, int high)
  * @array: array to be sorted
  * @low: lower bound of the array
  * @high: upper bound of the array
+ * @size: the size of array to be sorted
  */
-void qs(int *array, int low, int high)
+void qs(int *array, int low, int high, size_t size)
 {
 	int p;
 
 	if (low < high)
 	{
-		p = partition(array, low, high);
-		qs(array, low, p - 1);
-		qs(array, p + 1, high);
+		p = partition(array, low, high, size);
+		qs(array, low, p - 1, size);
+		qs(array, p + 1, high, size);
 	}
 }
 /**
@@ -70,6 +71,8 @@ void quick_sort(int *array, size_t size)
 {
 	int high;
 
+	if (size < 2)
+		return;
 	high = size - 1;
-	qs(array, 0, high);
+	qs(array, 0, high, size);
 }

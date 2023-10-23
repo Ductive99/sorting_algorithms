a1ce4a96dcf34a132deec1dc8d4e9527ca4ec977
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-23 16:54:39 +0100

    added counting sort algorithm

---
+++ 102-counting_sort.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * find_k - finds k (the max key value) of an array of integers
+ *
+ * @array: array to look in
+ * @size: size of the array
+ *
+ * Return: max key value
+*/
+int find_k(int *array, size_t size)
+{
+	size_t i;
+	int max;
+
+	if (!array)
+		return (0);
+
+	max = array[0];
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * counting_sort - sorts integers in ascending order
+ *
+ * Description: non-comparision sorting algorithm that counts
+ * the frequency of elements and places them accordingly.
+ *
+ * @array: array of positive integers
+ * @size: size of the array
+*/
+void counting_sort(int *array, size_t size)
+{
+	size_t i, k;
+	int *countingArray, *output, max, index;
+
+	if (!array || size < 2)
+		return;
+
+	max = find_k(array, size);
+
+	countingArray = malloc((max + 1) * sizeof(int));
+	output = malloc(size * sizeof(int));
+	if (!countingArray || !output)
+		return;
+
+	k = max + 1;
+
+	for (i = 0; i < size; i++)
+		countingArray[array[i]] += 1;
+
+	for (i = 0; i < k; i++)
+		countingArray[i + 1] += countingArray[i];
+
+	for (i = k; i > 0; i--)
+		countingArray[i] = countingArray[i - 1];
+
+	countingArray[0] = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		index = array[i];
+		output[countingArray[index]] = array[i];
+		countingArray[index]++;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+
+	print_array(countingArray, k + 1);
+
+	free(countingArray);
+	free(output);
+}
